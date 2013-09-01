84adbc4b8aa47a75a57119eca840ec96dd948503
suchesqiu <x@btbtd.org>
2013-09-01 14:46:02 +0800

    修改api文档生成模式

--- tools/merge.comps.sh
+++ tools/merge.comps.sh
@@ -11,8 +11,15 @@ sh -c "cd $dir/comps/Panel && node nodejs_merge.js";
 sh -c "cd $dir/comps/Form && node nodejs_merge.js";
 sh -c "cd $dir/comps/LunarCalendar && node nodejs_merge.js";
 
-sleep 1s
+sleep 1s;
+
+sh -c "cd $dir && tar -cvpzf tmp.tgz common.js JC.js"
+rm $dir/common.js
+rm $dir/JC.js
 
 sh -c "cd $cur_dir && sh generate_api_docs.sh";
+
+sh -c "cd $dir && tar -xvpzf tmp.tgz -C .";
+rm $dir/tmp.tgz
 
 echo $dir;
