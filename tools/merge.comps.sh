73426922809ab7c7a9b9e19673995304cb052e55
suchesqiu <x@btbtd.org>
2013-09-01 14:21:16 +0800

    修改api文档生成模式

--- tools/merge.comps.sh
+++ tools/merge.comps.sh
@@ -3,11 +3,6 @@ cd ..
 dir=$PWD; 
 cd $cur_dir;
 
-
-sh -c "sh generate_api_docs.sh";
-
-sleep 3s
-
 echo "sleep done";
 
 sh -c "node nodejs_merge.js";
@@ -15,5 +10,9 @@ sh -c "cd $dir/comps/Calendar && node nodejs_merge.js";
 sh -c "cd $dir/comps/Panel && node nodejs_merge.js";
 sh -c "cd $dir/comps/Form && node nodejs_merge.js";
 sh -c "cd $dir/comps/LunarCalendar && node nodejs_merge.js";
+
+sleep 1s
+
+sh -c "cd $cur_dir && sh generate_api_docs.sh";
 
 echo $dir;
