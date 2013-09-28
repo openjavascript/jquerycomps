8f02baba06f5fa1cf6eb4128f18768f143aff612
suchesqiu <x@btbtd.org>
2013-09-28 23:32:01 +0800

    tmp

--- comps/AjaxUpload/_demo/data/handler.php
+++ comps/AjaxUpload/_demo/data/handler.php
@@ -6,7 +6,7 @@
     }
 
     $r['data']['name'] = 'test.jpg';
-    $r['data']['url'] = '/ignore/JQueryComps_dev/comps/AjaxUpload/_demo/data/images/test.jpg';
+    $r['data']['url'] = './data/images/test.jpg';
 
     echo json_encode( $r );
 ?>
