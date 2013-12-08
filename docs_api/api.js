15212cbb885057a74f7f587c1567c71f50ea9397
openjavascript <openjavascript@btbtd.org>
2013-12-08 15:03:02 +0800

    Bizs.DisableLogic rename to Bizs.ChangeLogic, add JC.TableFreeze

--- docs_api/api.js
+++ docs_api/api.js
@@ -23,6 +23,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "JC.Slider",
         "JC.Suggest",
         "JC.Tab",
+        "JC.TableFreeze",
         "JC.Tips",
         "JC.Tree",
         "JC.Valid",
