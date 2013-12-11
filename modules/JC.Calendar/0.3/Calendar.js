69bda871b7ca1b84895065e51a2c8cf6cef3bbb2
openjavascript <openjavascript@btbtd.org>
2013-12-11 17:36:33 +0800

    tmp

--- modules/JC.Calendar/0.3/Calendar.js
+++ modules/JC.Calendar/0.3/Calendar.js
@@ -27,6 +27,7 @@
      *          <p><b>week:</b> 周日历</p>
      *          <p><b>month:</b> 月日历</p>
      *          <p><b>season:</b> 季日历</p>
+     *          <p><b>year:</b> 年日历</p>
      *          <p><b>monthday:</b> 多选日期日历</p>
      *      </dd>
      *
@@ -113,6 +114,12 @@
                 {
                     this._model = new Calendar.SeasonModel( _selector );
                     this._view = new Calendar.SeasonView( this._model );
+                    break;
+                }
+            case 'year': 
+                {
+                    this._model = new Calendar.YearModel( _selector );
+                    this._view = new Calendar.YearView( this._model );
                     break;
                 }
             case 'monthday':
@@ -2339,6 +2346,189 @@
                 JC.Calendar.hide();
             }
     });
+    //
+    /// YEAR CODE
+    //
+    /**
+     * 自定义周弹框的模板HTML
+     * @for         JC.Calendar
+     * @property    yearTpl
+     * @type        string
+     * @default     empty
+     * @static
+     */
+    JC.Calendar.yearTpl = '';
+
+    function YearModel( _selector ){
+        this._selector = _selector;
+    }
+    JC.Calendar.YearModel = YearModel;
+    
+    function YearView( _model ){
+        this._model = _model;
+    }
+    JC.Calendar.YearView = YearView;
+
+    JC.Calendar.clone( YearModel, YearView );
+
+    JC.f.mergeObject( YearModel.prototype, {
+        layout: 
+            function(){
+                var _r = $('#UXCCalendar_year');
+
+                if( !_r.length ){
+                    _r = $( JC.Calendar.yearTpl || this.tpl ).hide();
+                    _r.attr('id', 'UXCCalendar_year').hide().appendTo( document.body );
+                 }
+                return _r;
+            }
+
+        , tpl:
+            [
+            '<div id="UXCCalendar_year" class="UXCCalendar UXCCalendar_week UXCCalendar_year" >'
+            ,'    <div class="UHeader">'
+            ,'        <button type="button" class="UButton UNextYear">&nbsp;&gt;&gt;&nbsp;</button>'
+            ,'        <button type="button" class="UButton UPreYear">&nbsp;&lt;&lt;&nbsp;</button>'
+            ,'        <select class="UYear" style=""></select>'
+            ,'    </div>'
+            ,'    <table class="UTable UTableBorder">'
+            ,'        <tbody></tbody>'
+            ,'    </table>'
+            ,'    <div class="UFooter">'
+            ,'        <button type="button" class="UConfirm">确定</button>'
+            ,'        <button type="button" class="UClear">清空</button>'
+            ,'        <button type="button" class="UCancel">取消</button>'
+            ,'    </div>'
+            ,'</div>'
+            ].join('')
+
+        , month: 
+            function(){
+                var _r = 0, _tmp, _date;
+                ( _tmp = this.layout().find('td.cur a[dstart]') ).length
+                    && ( _date = new Date() )
+                    && (
+                            _date.setTime( _tmp.attr('dstart') )
+                            , _r = _date.getMonth()
+                       )
+                    ;
+                return _r;
+            }
+
+        , selectedDate:
+            function(){
+                var _r, _tmp, _item;
+                _tmp = this.layout().find('td.cur');
+                _tmp.length 
+                    && !_tmp.hasClass( 'unable' )
+                    && ( _item = _tmp.find('a[dstart]') )
+                    && ( 
+                            _r = { 'start': new Date(), 'end': new Date() }
+                            , _r.start.setTime( _item.attr('dstart') ) 
+                            , _r.end.setTime( _item.attr('dend') ) 
+                        )
+                    ;
+                return _r;
+            }
+    });
+
+    JC.f.mergeObject( YearView.prototype, {
+
+        _buildBody:
+            function( _dateo ){
+                var _p = this
+                    , _date = _dateo.date
+                    , _layout = _p._model.layout()
+                    , today = new Date( new Date().getFullYear(), new Date().getMonth(), new Date().getDate() ).getTime()
+                    , nextCount = 0
+                    , _ls = [], _class, _data, _title, _sdate, _edate, _year = _date.getFullYear()
+                    , _rows = 4
+                    , ipt = JC.Calendar.lastIpt
+                    , currentcanselect = JC.f.parseBool( ipt.attr('currentcanselect') )
+                    ;
+
+                    if( _dateo.maxvalue && currentcanselect ){
+                        var _m = _dateo.maxvalue.getMonth() + 1, _md;
+
+                        if( _m % 3 !== 0 ){
+                            _dateo.maxvalue.setDate( 1 );
+                            _dateo.maxvalue.setMonth( _m + ( 3 - ( _m % 3 ) - 1 ) );
+                        }
+                        _dateo.maxvalue.setDate( JC.f.maxDayOfMonth( _dateo.maxvalue ) );
+                    }
+
+                    _ls.push('<tr>');
+                    for( var i = 1, j = 4; i <= j; i++ ){
+                        _sdate = new Date( _year, i * 3 - 3, 1 ); 
+                        _edate = new Date( _year, i * 3 - 1, 1 );
+                        _edate.setDate( JC.f.maxDayOfMonth( _edate ) );
+
+                        var _cnUnit = JC.Calendar.cnUnit.charAt( i % 10 );
+                        i > 10 && ( _cnUnit = "十" + _cnUnit );
+
+                        _title = JC.f.printf( "{0}年 第{1}季度\n开始日期: {2} (周{4})\n结束日期: {3} (周{5})"
+                                    , _year
+                                    , JC.Calendar.getCnNum( i )
+                                    , JC.f.formatISODate( _sdate )
+                                    , JC.f.formatISODate( _edate )
+                                    , JC.Calendar.cnWeek.charAt( _sdate.getDay() % 7 )
+                                    , JC.Calendar.cnWeek.charAt( _edate.getDay() % 7 )
+                                    );
+
+                        _class = [];
+
+                        if( _dateo.minvalue && _sdate.getTime() < _dateo.minvalue.getTime() ) 
+                            _class.push( 'unable' );
+                        if( _dateo.maxvalue && _edate.getTime() > _dateo.maxvalue.getTime() ){
+                            _class.push( 'unable' );
+                        }
+
+                        if( _date.getTime() >= _sdate.getTime() && _date.getTime() <= _edate.getTime() ) _class.push( 'cur' );
+                        if( today >= _sdate.getTime() && today <= _edate.getTime() ) _class.push( 'today' );
+
+
+                        _ls.push( JC.f.printf( '<td class="{0}"><a href="javascript:" title="{1}"'+
+                                        ' dstart="{3}" dend="{4}" month="{5}" >{2}季度</a></td>'
+                                    , _class.join(' ')
+                                    , _title
+                                    , _cnUnit
+                                    , _sdate.getTime()
+                                    , _edate.getTime()
+                                    , i
+                                ));
+                        if( i % 2 === 0 && i != j ) _ls.push( '</tr><tr>' );
+                    }
+                    _ls.push('</tr>'); 
+     
+                    _layout.find('table.UTableBorder tbody' ).html( $( _ls.join('') ) );
+            }
+
+        , updateSelected: 
+            function( _userSelectedItem ){
+                var _p = this, _dstart, _dend, _tmp;
+                if( !_userSelectedItem ){
+                    _tmp = this._model.selectedDate();
+                    _tmp && ( _dstart = _tmp.start, _dend = _tmp.end );
+                }else{
+                    _userSelectedItem = $( _userSelectedItem );
+                    _tmp = JC.f.getJqParent( _userSelectedItem, 'td' );
+                    if( _tmp && _tmp.hasClass('unable') ) return;
+                    _dstart = new Date(); _dend = new Date();
+                    _dstart.setTime( _userSelectedItem.attr('dstart') );
+                    _dend.setTime( _userSelectedItem.attr('dend') );
+                }
+                if( !( _dstart && _dend ) ) return;
+
+                /*
+                _p._model.selector().val( JC.f.printf( '{0} 至 {1}', JC.f.formatISODate( _dstart ), JC.f.formatISODate( _dend ) ) );
+                */
+                _p._model.selector().val( _p._model.fullFormat( _p._model.dateFormat( _dstart ), _p._model.dateFormat( _dend ) ) );
+                $(_p).trigger( 'TriggerEvent', [ JC.Calendar.Model.UPDATE, 'year', _dstart, _dend ] );
+
+                JC.Calendar.hide();
+            }
+    });
+
     //
     /// MONTHDAY CODE
     //
