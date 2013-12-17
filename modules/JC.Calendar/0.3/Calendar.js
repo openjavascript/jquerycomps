09bc0f1e9f73a48f5c21850a5173c355d3336a2d
openjavascript <openjavascript@btbtd.org>
2013-12-17 16:07:39 +0800

    tmp

--- modules/JC.Calendar/0.3/Calendar.js
+++ modules/JC.Calendar/0.3/Calendar.js
@@ -2,6 +2,16 @@
     define( [ 'JC.Calendar.date', 'JC.Calendar.week', 'JC.Calendar.month'
                 , 'JC.Calendar.season', 'JC.Calendar.year', 'JC.Calendar.monthday'
             ], function(){
+    if( JC.use && JC.PATH ){
+        JC.use( [ 
+                JC.PATH + 'comps/Calendar/Calendar.date.js'
+                , JC.PATH + 'comps/Calendar/Calendar.week.js' 
+                , JC.PATH + 'comps/Calendar/Calendar.month.js' 
+                , JC.PATH + 'comps/Calendar/Calendar.season.js' 
+                , JC.PATH + 'comps/Calendar/Calendar.year.js' 
+                , JC.PATH + 'comps/Calendar/Calendar.monthday.js' 
+        ].join() );
+    }
 });}( typeof define === 'function' && define.amd ? define : 
         function ( _name, _require, _cb ) { 
             typeof _name == 'function' && ( _cb = _name );
