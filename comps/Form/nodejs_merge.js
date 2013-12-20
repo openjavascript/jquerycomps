e158b4db2c40a4d2acf4004f2f109215965f1279
openjavascript <openjavascript@btbtd.org>
2013-12-20 17:26:13 +0800

    up JC.Form

--- comps/Form/nodejs_merge.js
+++ comps/Form/nodejs_merge.js
@@ -1,28 +1,29 @@
-var fs = require('fs');
+var _fs = require('fs');
 
-var dir = __dirname + '/';
+var _dir = __dirname + '/'
+    , _paths = []
+    , _contents = []
+    , _outputName = 'Form.js'
+    ;
 
-var path1 = dir + 'JC.Form.js';
-//var path2 = dir + 'JC.Form.initCheckAll.js';
-var path4 = dir + 'JC.Form.initAutoFill.js';
-var path5 = dir + 'JC.Form.initNumericStepper.js';
+    _paths.push( _dir + 'Form.default.js' );
+    _paths.push( _dir + 'Form.initAutoFill.js' );
+    _paths.push( _dir + 'Form.initNumericStepper.js' );
 
-var outPath = dir + 'Form.js';
+for( var i = 0, j = _paths.length; i < j; i++ ){
+    if( ! _fs.existsSync( _paths[i] )  ) return;
+}
 
-if( !( fs.existsSync( path1 ) 
-        //&& fs.existsSync( path2 ) 
-        && fs.existsSync( path5 ) 
-        && fs.existsSync( path4 )  ) ) return;
+for( var i = 0, j = _paths.length; i < j; i++ ){
+    _contents.push( _fs.readFileSync( _paths[i], 'utf8') );
+}
 
-var tmp = [];
-    tmp.push( fs.readFileSync( path1, 'utf8') );
-    //tmp.push( fs.readFileSync( path2, 'utf8') );
-    tmp.push( fs.readFileSync( path4, 'utf8') );
-    tmp.push( fs.readFileSync( path5, 'utf8') );
+_fs.writeFileSync( _dir + _outputName, _contents.join(';\n') );
 
-fs.writeFileSync( outPath, tmp.join(';\n\n') );
+for( var i = 0, j = _paths.length; i < j; i++ ){
+    _fs.unlinkSync( _paths[i] );
+}
 
-fs.unlinkSync( path1 );
-//fs.unlinkSync( path2 );
-fs.unlinkSync( path4 );
-fs.unlinkSync( path5 );
+console.log( 'merge done: ' + _outputName );
+
+
