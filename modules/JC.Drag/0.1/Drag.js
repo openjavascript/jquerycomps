6dbd94474b5b0a116bd05dbd41c183a81ad060b5
openjavascript <openjavascript@btbtd.org>
2013-12-27 18:19:54 +0800

    tmp

--- modules/JC.Drag/0.1/Drag.js
+++ modules/JC.Drag/0.1/Drag.js
@@ -315,6 +315,17 @@
                 });
             }
     });
+    /**
+     * 判断两个矩形是否有交集
+     */
+    function intersectRect( r1, r2 ) {
+        return !(
+                    r2.left > r1.right || 
+                    r2.right < r1.left || 
+                    r2.top > r1.bottom ||
+                    r2.bottom < r1.top
+                );
+    }
 
     /*
     $(document).ready( function(){
