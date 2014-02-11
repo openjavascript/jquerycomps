cbfe0b7b8ea30aacfac2fe3cd3219011493aeb2e
openjavascript <openjavascript@btbtd.org>
2014-02-11 19:11:07 +0800

    update index page

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-    include_once dirname( __FILE__ ) . '/tools/php/filelist.php';
+    //include_once dirname( __FILE__ ) . '/tools/php/filelist.php';
+    header( "Location: ./docs_api/index.html");;
+    exit;
 ?>
