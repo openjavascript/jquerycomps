58b1e489db34ee5e19812ee3ad7d1ce69f8ed666
openjavascript <openjavascript@btbtd.org>
2014-02-13 09:46:02 +0800

    tmp

--- docs_api/api.js
+++ docs_api/api.js
@@ -1,7 +1,6 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "DEV.Bizs.FormLogic",
         "DEV.JC.ImageCutter",
         "JC.AjaxUpload",
         "JC.AutoChecked",
