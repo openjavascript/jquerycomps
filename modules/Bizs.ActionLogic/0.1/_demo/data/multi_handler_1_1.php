0c94d3f1705198618b6576a3ea76bc3c5e6a2879
openjavascript <openjavascript@btbtd.org>
2014-05-09 01:59:01 +0800

    tmp

---
+++ modules/Bizs.ActionLogic/0.1/_demo/data/multi_handler_1_1.php
@@ -0,0 +1,21 @@
+<?php
+    $r = array( 'errorno' => 0, 'errmsg' => '', 'data' => array () );
+
+
+    if( isset( $_REQUEST['errorno'] ) ){
+        $r['errorno'] = (int)$_REQUEST['errorno'];
+    }else{
+        $rnd = rand( 0, 1 );
+        $r['errorno'] = $rnd;
+    }
+
+    $r[ 'data' ][ 'balAction' ] = array(
+        'type' => 'ajaxaction'
+        , 'url' =>  './data/multi_handler_1_2.php?errorno=0'
+        , 'msg' =>  "单笔合同已撤销，是否一起撤销该单笔合同对应的所有上线任务？"
+        , 'returnurl' => '?actiondone=1'
+        , 'btnText' => array( 'confirm' => '是, 撤销', 'cancel' => '否, 取消' )
+    );
+
+    echo json_encode( $r );
+?>
