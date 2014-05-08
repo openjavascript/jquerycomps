0b094d525cafbaf1d14668e17bf099a35c010ff1
openjavascript <openjavascript@btbtd.org>
2014-05-08 17:44:16 +0800

    tmp

--- modules/JC.DragSelect/0.1/DragSelect.js
+++ modules/JC.DragSelect/0.1/DragSelect.js
@@ -73,6 +73,11 @@
             return _r;
         };
 
+    DragSelect.AR_IX = 
+        function( _ar, _ix, _def ){
+            return _ar[ _ix ] || _ar[ 0 ] || _def || '';
+        };
+
     JC.BaseMVC.build( DragSelect );
 
     JC.f.extendObject( DragSelect.prototype, {
@@ -88,56 +93,83 @@
                 _p.on( 'inited', function(){
                 });
 
-                _p.selector().delegate( _p._model.itemsClass(), 'click', function( _evt ){
-                    JC.log( 'click', JC.f.ts() );
-                });
+                $.each( _p._model.itemsClassAr(), function( _IX, _ITEM_CLASS ){
 
-                _p.selector().delegate( _p._model.itemsClass(), 'mousedown', function( _evt ){
-                    _evt.stopPropagation();
-                    _evt.preventDefault();
-                    JC.log( 'mousedown', JC.f.ts() );
-                    _p._model.clearCache();
-                    _p._model.selectReady( true );
-                    _p._bindActionEvent();
-                    _p.trigger( 'item_find', [ this ] );
-                });
-
-                _p.selector().delegate( _p._model.itemsClass(), 'mouseover', function( _evt ){
-                    if( !_p._model.selectReady() ) return;
-                    _p.trigger( 'item_find', [ this ] );
-                });
-
-                _p.on( 'item_find', function( _evt, _sp ){
-                    //_sp.toggleClass( _p._model.tempSelectClass() );
-                    _sp = $( _sp );
-                    _sp.toggleClass( _p._model.tempSelectClass() );
-                });
-
-                _p.on( 'find_elements', function( _evt, _src, _tar, _evtType ){
-                    JC.log( 'find_elements', _src.mouseX, _src.mouseY, _tar.mouseX, _tar.mouseY, JC.f.ts(), _tar.evt.type );
-                    var _spoint = new Point( _src.mouseX, _src.mouseY )
-                        , _epoint = new Point( _tar.mouseX, _tar.mouseY )
-                        , _rects = _p._model.itemsToRect()
-                        , _find = []
+                    var _selectClass = DragSelect.AR_IX( _p._model.selectClassAr(), _IX )
+                        , _unselectClass = DragSelect.AR_IX( _p._model.unselectClassAr(), _IX )
                         ;
-                    $.each( _rects, function( _ix, _item ){
-                        if( lineIntersectsRect( _spoint, _epoint, _item ) ){
-                            _find.push( _item );
+                    JC.log( _IX, _ITEM_CLASS, _selectClass, _unselectClass );
+
+                    _p.selector().delegate( _ITEM_CLASS, 'click', function( _evt ){
+                        JC.log( 'click', JC.f.ts(), _ITEM_CLASS );
+                    });
+
+                    _p.selector().delegate( _ITEM_CLASS, 'mousedown', function( _evt ){
+                        var _sp = $( this );
+                        _p._model.clearCache();
+                        _p._model.selectReady( _IX, true );
+                        _p._bindActionEvent( _IX );
+                        _p._model.prevItem( null );
+                        _p._model.countKey( true );
+                        _p.trigger( 'item_find', [ _sp, _IX ] );
+                        //_p._model.prevItem( _sp );
+                        JC.log( 'mousedown', _IX, _ITEM_CLASS, JC.f.ts() );
+                    });
+
+
+                    _p.selector().delegate( _ITEM_CLASS, 'mouseover', function( _evt ){
+                        if( !_p._model.selectReady( _IX ) ) return;
+                        JC.log( 'mouseover', _IX, _ITEM_CLASS );
+                        var _sp = $( this ), _count;
+                        if( !_sp.data( _p._model.countKey() ) ){
+                            _sp.data( _p._model.countKey(),  DragSelect.Model.UNI_COUNT++ )
+                        }else{
+                            _count = _sp.data( _p._model.countKey() );
+                            if( ( DragSelect.Model.UNI_COUNT - 2 ) == _count && _p._model.prevItem() ){
+                                _p._model.prevItem().data( _p._model.countKey(), 0 );
+                                _p.trigger( 'unselect_item', _p._model.prevItem(), _IX );
+                            }
+                        }
+                        _p.trigger( 'item_find', [ _sp, _IX ] );
+
+                        //_p._model.prevItem( _sp );
+                        //_p.trigger( 'item_find', [ _sp ] );
+                        JC.log( _sp.data( _p._model.countKey() ) );
+                    });
+
+                    _p.on( 'item_find', function( _evt, _sp, _ix ){
+                        if( _ix !== _IX ) return;
+                       
+                        if( _sp.hasClass( _selectClass ) ){
+                            _p.trigger( 'unselect_item', [ _sp, _ix ] );
+                        }else{
+                            _p.trigger( 'select_item', [ _sp, _ix ] );
                         }
                     });
-                    _evtType && _p.trigger( _evtType, [ _find, _src, _tar ] );
+
+                    _p.on( 'select_item', function( _evt, _sp, _ix ){
+                        JC.log( _IX, _ix, 'select_item', JC.f.ts() )
+                        if( _ix !== _IX ) return;
+                        _sp = $( _sp );
+
+                        _selectClass && _sp.addClass( _selectClass );
+
+                        _unselectClass && _sp.removeClass( _unselectClass );
+                    });
+
+                    _p.on( 'unselect_item', function( _evt, _sp, _ix ){
+                        JC.log( _IX, _ix, 'unselect_item', JC.f.ts() )
+                        if( _ix !== _IX ) return;
+                        if( _ix !== _IX ) return;
+                        _sp = $( _sp );
+
+                        _selectClass && _sp.removeClass( _selectClass );
+                        _unselectClass && _sp.addClass( _unselectClass );
+                    });
+
                 });
 
-                _p.on( 'finding', function( _evt, _items, _src, _tar ){
-                    if( !_items.length ) return;
-                    _p._view.setFindingStyle( _items );
-                    JC.log( 'finding', JC.f.ts(), _items.length );
-                });
-
-                _p.on( 'find_done', function( _evt, _items, _src, _tar ){
-                    if( !_items.length ) return;
-                    _p._view.cleanFindingStyle();
-                });
+                return;
             }
 
         , _inited:
@@ -147,7 +179,7 @@
             }
 
         , _bindActionEvent:
-            function(){
+            function( _IX ){
                 var _p = this;
 
                 _jwin.off( 'mouseup', _upEvent );
@@ -156,7 +188,7 @@
 
                 function _upEvent( _evt ){
                     JC.log( 'upevent', JC.f.ts() );
-                    _p._model.selectReady( false );
+                    _p._model.selectReady( _IX, false );
                     _jwin.off( 'mouseup', _upEvent );
                 }
             }
@@ -171,83 +203,61 @@
                 JC.log( 'DragSelect.Model.init:', new Date().getTime() );
             }
 
-        , selectReady: 
+        , prevItem:
             function( _setter ){
-                typeof _setter != 'undefined' && ( this._selectReady = _setter );
-                return this._selectReady;
+                typeof _setter != 'undefined' && ( this._prevItem = _setter );
+                return this._prevItem;
+            }
+
+        , countKey:
+            function( _new ){
+                var _p = this;
+                ( !_p._countKey || _new ) && ( _p._countKey = 'CDS_' + new Date().getTime() );
+                return _p._countKey;
+            }
+
+        , selectReady: 
+            function( _IX, _setter ){
+                !this._selectReady && ( this._selectReady = {} );
+                typeof _setter != 'undefined' && ( this._selectReady[ _IX ] = _setter );
+                return this._selectReady[ _IX ];
             }
 
         , items: function(){ return this.selectorProp( 'cdsItems'); }
         , itemsClass: function(){ return this.attrProp( 'cdsItems'); }
-
-        , itemsToRect:
-            function(){
-                var _p = this;
-
-                if( !_p._rects ){
-                    _p._rects = [];
-
-                    _p.items().each( function(){
-                        var _sp = $( this ), _rect = selectorToRectangle( _sp );
-                        _rect.item = _sp;
-                        _p._rects.push( _rect );
-                    });
-
-                }
-                return _p._rects;
-            }
-
         , selectClass: function(){ return this.attrProp( 'cdsSelectClass'); }
         , unselectClass: function(){ return this.attrProp( 'cdsUnselectClass'); }
-        , tempSelectClass: function(){ return this.attrProp( 'cdsTempSelectClass'); }
 
-        , evtData:
-            function( _evt ){
-                var _p = this
-                    , _roffset = _p.relativeParent() ? _p.relativeParent().offset() : { 'left': 0, 'top': 0 }
-                    , _r = {
-                        evt: _evt
-                        , 'mouseX': _evt.pageX
-                        , 'mouseY': _evt.pageY
-                        , 'maxXFix': -1
-                        , 'maxYFix': -1
-                        , 'relativeFixX': _roffset.left
-                        , 'relativeFixY': _roffset.top
-
-                    };
-
-                return _r;
-            }
-
-        , relativeParent:
+        , itemsAr:
             function(){
-                var _p = this;
-                if( !_p._relativeParent == 'undefined' ){
-                    _p._relativeParent = null;
-                    var _tmp = _p.selector();
-                    while( (_tmp = $( _tmp.parent() ) ).length ){
-                        if( /body|html/i.test( _tmp.prop( 'nodeName' ) ) ) break;
-                        if( ( _tmp.css( 'position' ) || '' ).toLowerCase() == 'relative' ){
-                            _p._relativeParent = _tmp;
-                            break;
-                        }
-                    }
-                }
-                return _p._relativeParent;
+                !this._itemsAr && ( this._itemsAr = this.items().split( this.itemDelimiter() ) );
+                return this._itemsAr;
             }
+
+        , itemsClassAr:
+            function(){
+                !this._itemsClassAr && ( this._itemsClassAr = this.itemsClass().split( this.itemDelimiter() ) );
+                return this._itemsClassAr;
+            }
+
+        , selectClassAr:
+            function(){
+                !this._selectClassAr && ( this._selectClassAr = this.selectClass().split( this.itemDelimiter() ) );
+                return this._selectClassAr;
+            }
+
+        , unselectClassAr:
+            function(){
+                !this._unselectClassAr && ( this._unselectClassAr = this.unselectClass().split( this.itemDelimiter() ) );
+                return this._unselectClassAr;
+            }
+
+        , itemDelimiter: function(){ return this.attrProp( 'cdsItemDelimiter' ) || '||'; }
 
         , clearCache:
             function(){
                 this._relativeParent = null;
-                this.prevFinding( null );
             }
-
-        , prevFinding:
-            function( _setter ){
-                typeof _setter != 'undefined' && ( this._prevFinding = _setter );
-                return this._prevFinding;
-            }
-
     });
 
     JC.f.extendObject( DragSelect.View.prototype, {
@@ -255,28 +265,7 @@
             function(){
                 JC.log( 'DragSelect.View.init:', new Date().getTime() );
             }
-
-        , setFindingStyle:
-            function( _items ){
-                var _p = this, _tmpClass = _p._model.tempSelectClass();
-                _p.cleanFindingStyle();
-                $.each( _items, function( _ix, _item ){
-                    _item.item.addClass( _tmpClass );
-                    JC.log( 'setFindingStyle', JC.f.ts(), _tmpClass );
-                });
-                _p._model.prevFinding( _items );
-            }
-
-        , cleanFindingStyle:
-            function(){
-                var _p = this, _tmpClass = _p._model.tempSelectClass();
-                if( !_p._model.prevFinding() ) return;
-                $.each( _p._model.prevFinding(), function( _ix, _item ){
-                    _item.item.removeClass( _tmpClass );
-                });
-            }
     });
-
 
     _jdoc.ready( function(){
         DragSelect.autoInit && DragSelect.init();
@@ -292,66 +281,4 @@
         , window
     )
 );
-
-    /**
-     * 判断一个点是否在矩形里面
-     */
-    function pointInRectangle( _point, _rect ){
-        return _point.x >= _rect.x 
-                && _point.x <= ( _rect.x + _rect.width )
-                && _point.y >= _rect.y
-                && _point.y <= ( _rect.y + _rect.height )
-                ;
-    }
-    /**
-     * 返回选择器的 矩形 位置
-     */
-    function selectorToRectangle( _selector ){
-        _selector = $( _selector );
-        var _offset = _selector.offset()
-            , _w = _selector.prop('offsetWidth')
-            , _h = _selector.prop('offsetHeight');
-
-        return {
-            x: _offset.left
-            , y: _offset.top
-            , width: _w
-            , height: _h
-        }
-    }
-
-    function Point( _x, _y ){
-        return { x: _x, y: _y };
-    }
-    /**
-     * 线条与矩形是否相交
-     */
-    function lineIntersectsRect( p1, p2, r){
-        return lineIntersectsLine(p1, p2, new Point(r.x, r.y), new Point(r.x + r.width, r.y)) ||
-               lineIntersectsLine(p1, p2, new Point(r.x + r.width, r.y), new Point(r.x + r.width, r.y + r.height)) ||
-               lineIntersectsLine(p1, p2, new Point(r.x + r.width, r.y + r.height), new Point(r.x, r.y + r.height)) ||
-               lineIntersectsLine(p1, p2, new Point(r.x, r.y + r.height), new Point(r.x, r.y)) ||
-               ( pointInRectangle( p1, r ) &&  pointInRectangle( p2, r ) );
-    }
-    /**
-     * 线条与线条是否相交
-     */
-    function lineIntersectsLine( l1p1, l1p2, l2p1, l2p2){
-        var q = (l1p1.y - l2p1.y) * (l2p2.x - l2p1.x) - (l1p1.x - l2p1.x) * (l2p2.y - l2p1.y);
-        var d = (l1p2.x - l1p1.x) * (l2p2.y - l2p1.y) - (l1p2.y - l1p1.y) * (l2p2.x - l2p1.x);
-
-        if( d == 0 ){
-            return false;
-        }
-
-        var r = q / d;
-        q = (l1p1.y - l2p1.y) * (l1p2.x - l1p1.x) - (l1p1.x - l2p1.x) * (l1p2.y - l1p1.y);
-        var s = q / d;
-
-        if( r < 0 || r > 1 || s < 0 || s > 1 ){
-            return false;
-        }
-
-        return true;
-    }
 
