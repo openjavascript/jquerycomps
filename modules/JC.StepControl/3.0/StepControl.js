6a949f8869c19546b10fdbf5de832b0d2f8701e3
openjavascript <openjavascript@btbtd.org>
2014-07-14 18:07:08 +0800

    tmp

--- modules/JC.StepControl/3.0/StepControl.js
+++ modules/JC.StepControl/3.0/StepControl.js
@@ -9,7 +9,7 @@
  *
  *<p><a href='https://github.com/openjavascript/jquerycomps' target='_blank'>JC Project Site</a>
  *   | <a href='http://jc2.openjavascript.org/docs_api/classes/JC.StepControl.html' target='_blank'>API docs</a>
- *   | <a href='../../modules/JC.StepControl/0.1/_demo' target='_blank'>demo link</a></p>
+ *   | <a href='../../modules/JC.StepControl/3.0/_demo' target='_blank'>demo link</a></p>
  *  
  *<h2>页面只要引用本脚本, 默认会处理 div class="js_compStepControl"</h2>
  *
