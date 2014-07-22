386b2ee537e533623c5cad1c0fceb14c1d7c9b20
pengjunkai <420838945@qq.com>
2014-07-22 16:26:29 +0800

    1

--- modules/JC.Cover/0.1/Cover.js
+++ modules/JC.Cover/0.1/Cover.js
@@ -15,39 +15,18 @@
  *  <h2>可用的 HTML attribute</h2>
  *
  *  <dl>
- *      <dt>itemcol = int, default = 4</dt>
- *      <dd>设置一行可以容纳的基本格数量<dd>
+ *      <dt>covercnt = string, default = ""</dt>
+ *      <dd>遮罩显示的内容，covertype="title"时，显示由
+ *      直接显示内容；covertype="selector"时，显示由select指向的内容<dd>
  *      
- *      <dt>itemwidth = int, default = 70</dt>
- *      <dd>设置基本格宽度<dd>
+ *      <dt>covertype = string, default = "title"</dt>
+ *      <dd>遮罩显示内容的类型<dd>
  *
- *      <dt>borderwidth = int, default = 0</dt>
- *      <dd>设置基本格边框宽度，默认无边框<dd>
+ *      <dt>coverlink = string, default = ""</dt>
+ *      <dd>点击cover时，跳转的地址URL<dd>
  *      
- *      <dt>boxcol = int default = 1</dt>
- *      <dd>设置内容格宽度所占基本格宽度的倍数</dd>
- *
- *      <dt>boxrow = int default = 1</dt>
- *      <dd>设置内容格高度所占基本格高度的倍数</dd>
- *
- *      <dt>itemcnt = string, default = ""</dt>
- *      <dd>设置内容格内显示的内容，若 'showtype' 属性设置为 'img' 时，该字段表示图片链接的URL地址</dd>
- *
- *      <dt>cntlink = string default = ""</dt>
- *      <dd>设置内容格点击时链接的URL地址，为空的时候代表不进行跳转</dd>
- *
- *      <dt>covertitle = string, default = ""</dt>
- *      <dd>设置内容格遮罩显示的内容</dd>
- *
- *      <dt>showtype = string, default = "title"</dt>
- *      <dd>设置内容格内展示的内容模式，可选项有：'title' 和 'img'</dd>
- *
- *      <dt>coverdir = int, default = 2</dt>
- *      <dd>设置遮罩出现的方向，可选项有：0-上、1-右、2-下、3-左</dd>
- *
- *      <dt>mod = int, default = 1</dt>
- *      <dd>设置遮罩显示模式，数值越大字号越大</dd>
-
+ *      <dt>coverpointer = boolean default = false</dt>
+ *      <dd>遮罩是否显示手型</dd>
  *  </dl> 
  *
  * @namespace   JC
@@ -59,32 +38,9 @@
  * @author  pengjunkai <pengjunkai@360.cn> | 75 Team
  * @example
         <h2>Default:</h2>
-        <div class="js_compCover" itemborder="1">
-            <ul>
-                <li itemcnt="习近平巴西国会演讲"></li>
-                <li itemcnt="李克强听取企业意见"></li>
-                <li itemcnt="副县长辞官"></li>
-                <li itemcnt="古巨基结婚"></li>
-                <li itemcnt="毛小兵被双开"></li>
-                <li itemcnt="张田欣赵智勇降级"></li>
-                <li itemcnt="MH17被击落"></li>
-                <li itemcnt="北京暴雨"></li>
-            </ul>
-        </div>
-        <h2>Custom Table Design:</h2>
-        <div class="js_compCover" itemwidth="90" itemborder="2" itemcol="3">
-            <ul>
-                <li itemcnt="习近平巴西国会演讲"></li>
-                <li itemcnt="李克强听取企业意见"></li>
-                <li itemcnt="副县长辞官"></li>
-                <li coverdir="3" itemcnt="古巨基结婚"></li>
-                <li coverdir="3" itemcnt="毛小兵被双开"></li>
-                <li coverdir="3" itemcnt="张田欣赵智勇降级"></li>
-                <li coverdir="0" itemcnt="MH17被击落"></li>
-                <li coverdir="0" itemcnt="北京暴雨"></li>
-                <li coverdir="0" itemcnt="博导诱奸女生事件"></li>
-            </ul>
-        </div>
+        <div class="js_compCover" covercnt="test" coverpointer="true" coverlink="http://www.so.com" style="height: 200px; width: 200px; background: red;"></div>
+        <h2>Some Text:</h2>
+        <div class="js_compCover" covercnt="Cover Text" coverdir="1" coverpointer="true" coverlink="http://www.so.com" style="height: 200px; width: 200px; background: red;">Some Text</div>
  */
     var _jdoc = $( document ), _jwin = $( window );
 
@@ -114,7 +70,6 @@
         function( _selector ){
             var _r = [];
             _selector = $( _selector || document );
-
             if( _selector.length ){
                 if( _selector.hasClass( 'js_compCover' )  ) {
                     _r.push( new Cover( _selector ) );
@@ -143,23 +98,20 @@
 
                 _p.on( 'inited', function() {
                 });
-                _selector.on( 'mouseover', function( e ) {
-                    var cnt = $( e.target );
-                    if( cnt.hasClass( _Model.COVER_CNT ) ){
-                        _view.coverItem( cnt.next() );
-                        _p.notification( _Model.COVERED, [ cnt.parent( '.' + _Model.COVER_BOX ), _p ] );
-                    }
+                _selector.on( 'mouseenter', function( e ) {
+                    var _t = $( e.target );
+                    _view.coverItem( _selector.children( '.' + _Model.COVER_COVER ) );
+                    _p.notification( _Model.COVERED, [ _t, _p ] );
                 } );
-                _selector.on( 'mouseleave', 'li', function( e ) {
-                    var target = $( e.target );
-                    !target.is( 'li' ) && ( target = target.parent( '.' + _Model.COVER_BOX ) );
-                    _view.coverBack( target.children( '.' + _Model.COVER_COVER ) );
+                _selector.on( 'mouseleave', function( e ) {
+                    var _t = $( e.target );
+                    _view.coverBack( _selector.children( '.' + _Model.COVER_COVER ) );
+                    _p.notification( _Model.LEAVED, [ _t, _p ] );
                 } );
-                _selector.on( 'click', 'li', function( e ) {
-                    var target = $( e.target );
-                    !target.is( 'li' ) && ( target = target.parent( '.' + _Model.COVER_BOX ) );
-                    _view.link( target );
-                    _p.notification( _Model.CLICKED, [ target, _p ] );
+                _selector.on( 'click', function( e ) {
+                    var _t = $( e.target );
+                    _view.link( _selector.children( '.' + _Model.COVER_COVER ) );
+                    _p.notification( _Model.CLICKED, [ _t, _p ] );
                 } );
             }
 
@@ -171,11 +123,7 @@
     
     var _Model = Cover.Model;
     /* Var */
-    _Model.COVER_CNT = 'cover-cnt';
     _Model.COVER_COVER = 'cover-cover';
-    _Model.COVER_BOX = 'cover-box';
-    _Model.COVER_BG = 'cover-itembg';
-    _Model.COVER_MOD = 'cover-mod';
     _Model.ANIMATE_SPEED = 200;
     _Model._instanceName = 'JCCover';
 
@@ -184,13 +132,14 @@
     /**
      * JC.Cover hover后遮罩遮挡后 selector 触发的事件
      * @event  coverCovered
-     * @param   {Event}         _evt
+     * @param   {Event}          _evt
+     * @param   {Target}         _target
      * @param   {CoverInstance}  _coverIns
      * @example
     <pre>
-    $( document ).delegate( "div.js_coverCoveredEvent", "coverCovered", function( _evt, _targetBox, _coverIns ) {
-        JC.log( 'item covered' );
-        JC.log( 'cover : ' + _targetBox.attr( 'covertitle' ) );
+    $( document ).delegate( "div.js_coverCoveredEvent", 
+        "coverCovered", function( _evt, _target, _coverIns ) {
+            JC.log( 'item covered' );
     } );
     </pre>
      */
@@ -200,104 +149,83 @@
      * JC.Cover 点击后 selector 触发的事件
      * @event  coverClicked
      * @param   {Event}         _evt
+     * @param   {Target}         _target
      * @param   {coverInstance}  _coverIns
      * @example
     <pre>
-    $( document ).delegate( "div.js_coverClickedEvent", "coverClicked", function( _evt, _targetBox, _coverIns ) {
-        JC.log( 'item clicked' );
-        JC.log( 'click : ' + _targetBox.attr( 'covertitle' ) );
+    $( document ).delegate( "div.js_coverClickedEvent", 
+        "coverClicked", function( _evt, _target, _coverIns ) {
+            JC.log( 'item clicked' );
     } );
     </pre>
      */
     _Model.CLICKED = 'coverClicked';
 
+    /**
+     * JC.Cover 鼠标离开后 selector 触发的事件
+     * @event  coverLeaved
+     * @param   {Event}         _evt
+     * @param   {Target}         _target
+     * @param   {coverInstance}  _coverIns
+     * @example
+    <pre>
+    $( document ).delegate( "div.js_coverLeavedEvent",
+        "coverLeaved", function( _evt, _target, _coverIns ) {
+            JC.log( 'item leaved' );
+    } );
+    </pre>
+     */
+    _Model.LEAVED = 'coverLeaved';
+
     JC.f.extendObject( _Model.prototype, {
         init:
             function() {
             }
-        , getItemCol:
+        , getCoverDir :
             function() {
-                return this.intProp( 'itemcol' ) || 4;
+                return this.attrProp( 'coverdir' );
             }
-        , getItemWidth:
+        , getCoverCnt :
             function() {
-                return this.intProp( 'itemwidth' ) || 70;
+                var covertype = this.getCoverType(),
+                    coverCnt = this.attrProp( 'covercnt' );
+                if( covertype == 'title' ) {
+                    return coverCnt;
+                } else if( covertype == 'selector' ) {
+                    return JC.f.parentSelector( this._selector, coverCnt ).html();
+                }
             }
-        , getItemMod:
-            function( item ) {
-                return this.intProp( item, 'mod' ) || 1;
-            }
-        , getShowType:
-            function( item ) {
-                return this.attrProp( item, 'showtype' ) || 'title';
-            }
-        , getItemCnt:
-            function( item ) {
-                return this.attrProp( item, 'itemcnt' );
-            }
-        , getCoverTitle:
-            function( item ) {
-                return this.attrProp( item, 'covertitle' );
-            }
-        , getCoverDir:
-            function( item ) {
-                return this.intProp( item, 'coverdir' ) || 2;
-            }
-        , getCntLink:
-            function( item ) {
-                return this.attrProp( item, 'cntlink' );
-            }
-        , getItemBorder:
+        , getCoverLink :
             function() {
-                return this.intProp( 'itemborder' ) || 0;
+                return this.attrProp( 'coverlink' );
             }
-        , getBoxCol:
-            function( item ) {
-                return this.intProp( item, 'boxcol' ) || 1;
+        , getCoverPointer :
+            function() {
+                return this.boolProp( 'coverpointer' );
             }
-        , getBoxRow:
-            function( item ) {
-                return this.intProp( item, 'boxrow' ) || 1;
+        , getCoverType :
+            function() {
+                return this.attrProp( 'covertype' ) || 'title';
             }
     });
 
     JC.f.extendObject( Cover.View.prototype, {
         init:
             function() {
-                var _p = this, item,
+                var _p = this,
                     _model = _p._model,
-                    _selector = _model.selector(),
-                    itemwidth = _model.getItemWidth(),
-                    itemCol = _model.getItemCol(),
-                    border = _model.getItemBorder();
-                _selector.css( 'width', itemCol * itemwidth + 'px' );
-                $.each( _selector.find( 'li' ), function( i , element ) {
-                    item = $( element );
-                    var showType = _model.getShowType( item ),
-                        itemCnt = _model.getItemCnt( item ),
-                        coverDir = _model.getCoverDir( item ),
-                        itemMod = _model.getItemMod( item ),
-                        boxCol = _model.getBoxCol( item ),
-                        boxRow = _model.getBoxRow( item ),
-                        coverTitle = _model.getCoverTitle( item );
-                    item.addClass( _Model.COVER_BOX + ' ' + _Model.COVER_MOD + itemMod )
-                        .css( 'border-width', border + 'px' );
-                    
-                    var itemContent = '<button class=" {0} " style=" {1} ">{2}</button>',
-                        itemCntStyle = JC.f.printf( 'width: {0}px; height: {1}px; ', 
-                            boxCol * itemwidth - border * 2, boxRow * itemwidth - border * 2 ),
-                        itemCntText = '&nbsp;';
-                    if( showType == 'title' ) {
-                        itemCntText = itemCnt;
-                    } else if( showType == 'img' ) {
-                        itemCntStyle += 'background: url( ' + itemCnt + ') no-repeat;';
-                    }
-                    itemContent = JC.f.printf( itemContent, _Model.COVER_CNT + ' ' + 
-                        _Model.COVER_BG + ( ( i & 1 ) == 0 ? '1 ' : '2 ' ), itemCntStyle, itemCntText );
-                    item.append( itemContent + '<button class="' + _Model.COVER_COVER + '">' + 
-                        ( coverTitle == '' ? itemCnt : coverTitle ) + '</button>' );
-                    _p.putCover( item.children( '.' + _Model.COVER_CNT ), coverDir );
-                } );
+                    cover = '',
+                    coverLink = _model.getCoverLink(),
+                    item = $( _model.selector() ), 
+                    cover = '<button class="{0}" style="{1}" {2}>{3}</button>',
+                    cntStyle = _model.getCoverPointer() ? ' cursor : pointer; ' : '',
+                    itemStyle = { 'overflow' : 'hidden' };
+                coverLink = coverLink ? ' linkurl="' + coverLink + '" ' : '';
+                item.css( 'position' ) == 'static' && ( itemStyle['position'] = 'relative' );
+                item.css( itemStyle );
+                item.append( JC.f.printf( cover, 
+                    _Model.COVER_COVER, cntStyle, coverLink, _model.getCoverCnt() ) );
+                _p.putCover( item, _model.getCoverDir() );
             }
         /**
         * 初始化遮罩
@@ -310,7 +238,7 @@
             function( target, dir ) {
                 var width = target[ 0 ].offsetWidth,
                     height = target[ 0 ].offsetHeight,
-                    cover = target.next(),
+                    cover = target.find( '.' + _Model.COVER_COVER ),
                     top, left;
                 if( ( dir & 1 ) == 0 ) {
                     left = 0;
@@ -320,7 +248,12 @@
                     left = ( dir == 1 ) ? width : parseInt( '-' + width );
                 }
                 cover.attr( 'covertop', top ).attr( 'coverleft', left )
-                    .css( { 'top' : top + 'px', 'left' : left + 'px' } );
+                    .css( {
+                        'top' : top + 'px',
+                        'left' : left + 'px',
+                        'height' : height + 'px',
+                        'width' : width + 'px'
+                    } );
             }
         , coverItem:
             function( cover ) {
@@ -334,10 +267,9 @@
             }
         , link:
             function( target ) {
-                var _p = this,
-                    _model = _p._model,
-                    link = _model.getCntLink( target );
-                link != '' && window.open( link );
+                
+                var linkurl = target.attr( 'linkurl' );
+                ( typeof linkurl != 'undefined' ) && window.open( linkurl );
             }
     });
 
