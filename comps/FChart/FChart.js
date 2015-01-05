a56a15476335edfd8540593a398e7e2e1c37a586
openjavascript <openjavascript@btbtd.org>
2015-01-05 18:48:29 +0800

    up JC.FChart

--- comps/FChart/FChart.js
+++ comps/FChart/FChart.js
@@ -264,11 +264,15 @@ JC.use && !jQuery.event.special.mousewheel && JC.use( 'plugins.jquery.mousewheel
         , 'histogram': 'Histogram'
 
         , 'vbar': 'VHistogram'
+        , 'hbar': 'VHistogram'
         , 'vhistogram': 'VHistogram'
 
         , 'column': 'ZHistogram'
         , 'zbar': 'ZHistogram'
         , 'zhistogram': 'ZHistogram'
+
+
+        , 'hcolumn': 'VZHistogram'
 
         , 'mix': 'MixChart'
 
